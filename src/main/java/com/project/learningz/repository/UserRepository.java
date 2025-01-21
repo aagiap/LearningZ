b3cd55b4bbd4f6c5cc016381d00ec31a138e6d9b
TruongNT <truongnt.studynwork@gmail.com>
2025-01-22 01:20:54 +0700

    forgot password functions and security

--- src/main/java/com/project/learningz/repository/UserRepository.java
+++ src/main/java/com/project/learningz/repository/UserRepository.java
@@ -4,7 +4,11 @@ package com.project.learningz.repository;
 import com.project.learningz.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
 
+    public User findByResetPasswordToken(String token);
 }
