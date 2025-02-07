d4d3f88764c6182cda6e30afbeaed540455fb22a
aagiap <gdarmor2.0@gmail.com>
2025-02-07 16:44:57 +0700

    done quizReview

---
+++ src/main/java/com/project/learningz/repository/QuizRepository.java
@@ -0,0 +1,51 @@
+package com.project.learningz.repository;
+
+import com.project.learningz.dto.QuizJoinToGradeDTO;
+import com.project.learningz.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+    @Query("SELECT new com.project.learningz.dto.QuizJoinToGradeDTO( q.id,q.timeLimit, q.totalQuestions, l.title, g.description, co.subject) " +
+            "FROM Quiz q " +
+            "JOIN q.lesson l " +
+            "JOIN l.chapter c " +
+            "JOIN c.course co " +
+            "JOIN co.grade g " +
+            "WHERE co.subject = :subject AND g.name = :description AND l.quizType = 'EXAM'")
+    List<QuizJoinToGradeDTO> findByCourseSubjectAndGradeDescription(
+            @Param("subject") String subject,
+            @Param("description") String description);
+
+    @Query("SELECT new com.project.learningz.dto.QuizJoinToGradeDTO(q.id, q.timeLimit, q.totalQuestions, l.title, g.description, co.subject) " +
+            "FROM Quiz q " +
+            "JOIN q.lesson l " +
+            "JOIN l.chapter c " +
+            "JOIN c.course co " +
+            "JOIN co.grade g " +
+            "WHERE co.subject = :subject AND l.quizType = 'EXAM'")
+    List<QuizJoinToGradeDTO> findByCourseSubject(@Param("subject") String subject);
+
+    @Query("SELECT new com.project.learningz.dto.QuizJoinToGradeDTO(q.id, q.timeLimit, q.totalQuestions, l.title, g.description, co.subject) " +
+            "FROM Quiz q " +
+            "JOIN q.lesson l " +
+            "JOIN l.chapter c " +
+            "JOIN c.course co " +
+            "JOIN co.grade g " +
+            "WHERE g.name = :grade AND l.quizType = 'EXAM'")
+    List<QuizJoinToGradeDTO> findByGrade(@Param("grade") String grade);
+
+    @Query("SELECT new com.project.learningz.dto.QuizJoinToGradeDTO(q.id, q.timeLimit, q.totalQuestions, l.title, g.description, co.subject) " +
+            "FROM Quiz q " +
+            "JOIN q.lesson l " +
+            "JOIN l.chapter c " +
+            "JOIN c.course co " +
+            "JOIN co.grade g " +
+            "WHERE q.id = :quizId")
+    QuizJoinToGradeDTO findByQuizId(@Param("quizId") Integer quizId);
+
+
+}
