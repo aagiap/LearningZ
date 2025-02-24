0999a044fccc936c7bf2832528b5460f33721815
Zakhan <khanhlghe186844@fpt.edu.vn>
2025-02-24 23:39:30 +0700

    post

---
+++ src/main/java/com/project/learningz/repository/PostRepository.java
@@ -0,0 +1,18 @@
+package com.project.learningz.repository;
+
+import com.project.learningz.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByUserId(Integer userId);
+    List<Post> findByCourseId(Integer courseId);
+    Page<Post> findAll(Pageable pageable);
+
+
+}
