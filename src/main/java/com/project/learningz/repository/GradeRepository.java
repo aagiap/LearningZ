7da937e1d7998185979c2d7b72788d3d0a7127ae
ktmchi <khuatchi2004dtt@gmail.com>
2025-03-03 16:42:52 +0700

    Add Question List 2

--- src/main/java/com/project/learningz/repository/GradeRepository.java
+++ src/main/java/com/project/learningz/repository/GradeRepository.java
@@ -15,5 +15,18 @@ public interface GradeRepository extends JpaRepository<Grade, Integer>, JpaSpeci
 
     @Query("SELECT g FROM Grade g WHERE g.id=?1")
     Grade findById(int gradeId);
+
+    @Query("""
+        SELECT DISTINCT g.name
+        FROM QuestionBank q
+                JOIN QuizQuestion qq ON q.id = qq.question.id
+                JOIN Quiz qu ON qq.quiz.id = qu.id
+                JOIN qu.lesson l
+                JOIN l.chapter ch
+                JOIN ch.course co
+                JOIN co.subject s
+                JOIN co.grade g
+    """)
+    List<String> getAllGrade();
 }
 
---
+++ src/main/java/com/project/learningz/service/QuestionExpertService.java
@@ -0,0 +1,36 @@
+package com.project.learningz.service;
+
+import com.project.learningz.dto.QuestionDetailDTO;
+import com.project.learningz.repository.QuestionExpertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionExpertService {
+
+    @Autowired
+    QuestionExpertRepository questionExpertRepository;
+
+    public Page<QuestionDetailDTO> getAllQuestion(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return questionExpertRepository.getAllQuestions(pageable);
+    }
+
+    public Page<QuestionDetailDTO> getAllQuestionByKeyword(String keyword, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return questionExpertRepository.searchAllQuestionsByKeyword(keyword, pageable);
+    }
+
+    public Page<QuestionDetailDTO> filterQuestion(String grade, String course, String subject, String chapter, String lesson, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return questionExpertRepository.filterQuestion(grade, course, subject, chapter, lesson, pageable);
+    }
+
+
+}
